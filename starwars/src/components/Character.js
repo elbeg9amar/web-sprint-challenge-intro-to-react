9c709b008328db21e43969a7174dbade326a654b
Amarbayasgalan elbeg <elbeg9amar@yahoo.com>
2020-08-14 17:56:57 -0400

    completed project

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,26 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    width:40%;
+    border-style: dotted;
+    margin: 0% 30% 0% 30% ;
+    background-color: black;
+    p {
+        font-size:25px;
+        text-align:center;
+        color:white;
+    }
+`
+function Character ({person} ) {
+    return (
+        <StyledCharacter>
+            <p className='name'>Name : {person.name}</p>
+            <p> BirthYear : {person['birth_year']}</p>
+            <button>+</button>
+        </StyledCharacter>
+    )
+}
+
+export default Character
